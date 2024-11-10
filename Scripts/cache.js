f3d4898f79ccf78674b29d9bc4b4680b01f91a4a
Nicolo Mariani <marianinicolo@itis-molinari.eu>
2024-11-10 12:45:14 +0100

    sistemazione cache

--- Scripts/cache.js
+++ Scripts/cache.js
@@ -1,41 +1,50 @@
 const upload = () => {
   return new Promise(()=>{
+    fetch("conf.json").then(r => r.json()).then(confData => {
       try{
-        fetch(url + "set", {
+        fetch(confData.url + "set", {
           method: "POST",
           headers: {
-              'Content-Type': 'application/json',
-              'key': token
+            'Content-Type': 'application/json',
+            'key': confData.token
           },
           body: JSON.stringify({
-              key: key,
-              value: data
+            key: confData.key,
+            value: data
           })
-    }).then(r => r.json())
-    .then(r => {console.log(r);})
-    }
-    catch(error){
-      reject(error)
-    }
+        }).then(r => r.json())
+          .then(r => {console.log(r);})
+      }
+      catch(error){
+        reject(error)
+      }
+    })    
   })
 }
 
 const download = () => {
   return new Promise((resolve,reject)=>{
-    try{
-      fetch(url + "get", {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          'key': token
-        },
-        body: JSON.stringify({
-          key: key
-        })
-      }).then(r => r.json())
-      .then(r => {resolve(r);})
+    fetch("conf.json").then(r => r.json()).then(confData => {
+      try{
+        fetch(confData.url + "get", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json',
+            'key': confData.token
+          },
+          body: JSON.stringify({
+            key: confData.key
+          })
+        }).then(r => r.json())
+        .then(r => {resolve(r);})
       }catch(error) {
         reject(error)
-    }
+      }
+    })
   })   
 }
+
+let data = {"A":"B"};
+upload();
+
+download().then(console.log);
