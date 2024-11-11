8c4c8269db664243c7b7ca51923be11ac90ea6ae
Tubos <loureiromanuel@itis-molinari.eu>
2024-11-11 13:46:50 +0100

    debug

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -1,3 +1,7 @@
-let getCoordinates = () => {
-    
+const getCoordinates = (luogo) => {
+    fetch("conf.json").then(r => r.json()).then(confData => {
+        fetch(confData.geoUrl.replace("$LUOGO", luogo)).then(r => r.json()).then(data => {
+            console.log(data);
+        })
+    })
 }
